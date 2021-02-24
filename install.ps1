f9b093d0fc3076594255417fe06d1f039a5c1edb
David Liu <david-khala@hotmail.com>
2021-02-24 16:30:23 +0800

    package steps into one

--- install.ps1
+++ install.ps1
@@ -1,4 +1,9 @@
-Set-ExecutionPolicy RemoteSigned
-Invoke-WebRequest https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.ps1 -OutFile install-temp.ps1
-./install-temp.ps1 -AcceptAllDefaults  
-Remove-Item install-temp.ps1
+try {
+    Invoke-WebRequest https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.ps1 -OutFile install-temp.ps1
+    ./install-temp.ps1 -AcceptAllDefaults  
+    Remove-Item install-temp.ps1
+}
+catch {
+    $_
+}
+
