368b3451c7eda8c9556e39b4f93f25172bba8374
DavidLiu <david.yx.liu@oracle.com>
2021-05-30 09:33:53 -0400

    default ${auth:-security_token}

--- compartment.sh
+++ compartment.sh
@@ -1,9 +1,10 @@
 set -e
+
 search() {
     local compartmentName=$1
     shift # shift 1 step to skip $1
     # get compartment ocid from compartment name
-    oci iam compartment list --all --compartment-id-in-subtree true --query "data[?name=='${compartmentName}'].id | [0]" --raw-output $@
-
+    oci iam compartment list --auth ${auth:-security_token} --all --compartment-id-in-subtree true --query "data[?name=='${compartmentName}'].id | [0]" --raw-output $@
 }
+
 $@
--- compute/instance.sh
+++ compute/instance.sh
@@ -1,15 +1,25 @@
 set -e
 CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 search() {
-    
+
     local instance=$1 # instance name
-    shift # shift 1 step to skip $1
+    shift             # shift 1 step to skip $1
     if [ -z $compartmentID ]; then
         local compartment=$1
-        compartmentID=$($CURRENT/../compartment.sh search $compartment)
+        export compartmentID=$($CURRENT/../compartment.sh search $compartment)
+        
         shift # shift 1 step to skip $1
     fi
-    
-    oci compute instance list --all --compartment-id $compartmentID --query "data[?name=='${instance}'].id | [0]" --raw-output $@
+
+    oci compute instance list --auth ${auth:-security_token} --all --compartment-id $compartmentID --query "data[?\"display-name\"=='${instance}'].id | [0]" --raw-output $@
 
 }
+list() {
+    if [ -z $compartmentID ]; then
+        local compartment=$1
+        export compartmentID=$($CURRENT/../compartment.sh search $compartment)
+        shift # shift 1 step to skip $1
+    fi
+    oci compute instance list --auth ${auth:-security_token} --all --compartment-id $compartmentID --query "data" $@
+}
+$@
