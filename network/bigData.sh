3db1958468fb5df62f2d82cb9d2f6de74d43c0be
David Liu <david.yx.liu@oracle.com>
2022-09-23 14:40:02 +0800

    Update bigData.sh

--- network/bigData.sh
+++ network/bigData.sh
@@ -3,7 +3,7 @@ map-public(){
   PRIVATE_IP=$1
   SUBNET_OCID=$2
   DISPLAY_NAME=$3
-  oci network public-ip create --display-name $DISPLAY_NAME --compartment-id `oci network private-ip list --subnet-id $SUBNET_OCID --ip-address $PRIVATE_IP | jq -r '.data[] | ."compartment-id"'` --lifetime "RESERVED" - private-ip-id `oci network private-ip list --subnet-id $SUBNET_OCID --ip-address $PRIVATE_IP | jq -r '.data[] | ."id"'`
+  oci network public-ip create --display-name $DISPLAY_NAME --compartment-id `oci network private-ip list --subnet-id $SUBNET_OCID --ip-address $PRIVATE_IP | jq -r '.data[] | ."compartment-id"'` --lifetime "RESERVED" --private-ip-id `oci network private-ip list --subnet-id $SUBNET_OCID --ip-address $PRIVATE_IP | jq -r '.data[] | ."id"'`
   
 
 }
